71741251ce89ef582731c6adb6c0ea60680aeee1
Angel F <angel12345leo@gmail.com>
2020-06-10 10:52:29 +0200

    first commit

---
+++ Assets/Scripts/Loading/LoadingController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+using UnityEngine.UI;
+
+public sealed class LoadingController : MonoBehaviour 
+{
+	private string loadTo = "MainMenu";
+	AsyncOperation async;
+	public float minimunLoadTime = 0.4f;
+	public Image progressBar;
+
+	void Start() 
+	{
+		loadTo = LoadingManager.Instance.SceneToLoad;
+		StartCoroutine(load());
+	}
+
+	void Update() 
+	{
+		if (async != null)
+		{
+			progressBar.fillAmount += async.progress * Time.deltaTime;
+		}
+
+		if (minimunLoadTime > 0)
+		{
+			minimunLoadTime -= Time.deltaTime;
+		} else {
+			minimunLoadTime = 0;
+		}
+		
+		if (minimunLoadTime == 0 || async.isDone)
+		{
+			ActivateScene();
+		}
+	}
+
+    IEnumerator load() 
+	{
+        async = SceneManager.LoadSceneAsync(loadTo);
+		async.allowSceneActivation = false;
+        yield return null;
+    }
+ 
+    void ActivateScene() 
+	{
+        async.allowSceneActivation = true;
+    }
+}
---
+++ Assets/Scripts/Loading/LoadingManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public sealed class LoadingManager : MonoBehaviour
+{
+	private string sceneToLoad = "MainMenu";
+
+	public static LoadingManager Instance;
+	
+	public string SceneToLoad
+	{
+		get { return sceneToLoad; }
+	}
+
+    void Awake()
+    {
+		if(Instance == null)
+		{
+			Instance = this;
+			
+			DontDestroyOnLoad(gameObject);
+		} else {
+			Destroy(gameObject);
+		}
+    }
+
+	void LoadTo(string sceneName)
+	{
+		Time.timeScale = 1;
+		sceneToLoad = sceneName;
+		SceneManager.LoadScene("Loading", LoadSceneMode.Single);
+	}
+
+	public static void LoadMainMenu()
+	{
+		if(Instance == null)
+		{
+			Debug.LogError("There is not a Loading Manager Instance in game.");
+			return;
+		}
+		
+		LoadingManager.Instance.LoadTo("MainMenu");
+	}
+
+	public static void LoadScene(string sceneName)
+	{
+		if(Instance == null)
+		{
+			Debug.LogError("There is not a Loading Manager Instance in game.");
+			return;
+		}
+		
+		LoadingManager.Instance.LoadTo(sceneName);
+	}
+
+	public static void ReloadScene()
+	{
+		if(Instance == null)
+		{
+			Debug.LogError("There is not a Loading Manager Instance in game.");
+			return;
+		}
+
+		LoadingManager.Instance.LoadTo(SceneManager.GetActiveScene().name);
+	}
+}
